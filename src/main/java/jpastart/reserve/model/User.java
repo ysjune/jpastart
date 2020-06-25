6d965abb9a76703143887ef9d27a608b162c9b42
ysjune <ysjune320@gmail.com>
2020-06-25 19:06:20 +0900

    chapter2 중간 커밋

---
+++ src/main/java/jpastart/reserve/model/User.java
@@ -0,0 +1,33 @@
+package jpastart.reserve.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    private String email;
+    private String name;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_date")
+    private Date createdDate;
+
+    protected User() {
+
+    }
+
+    @Builder
+    public User(final String email, final String name, final Date createdDate) {
+        this.email = email;
+        this.name = name;
+        this.createdDate = createdDate;
+    }
+
+}
